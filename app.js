b4f036dc67dfe65c082c3f21ac6dd3f7a6bc9293
Stephanie Cure <e.cure.ch@gmail.com>
2017-01-17 12:40:03 -0500

    Configurin express

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const app = express(); // creates an instance of an express application
+const chalk = require('chalk');
+const router = express.Router();
+const request = chalk.green;
+const morgan = require('morgan');
+//Loggin middleware
 
-app.get('/', function(req, res){
-  res.send('Welcome!');
+
+app.use(morgan('dev'))
+
+//a middleware sub-stack shows request info for any type of HTTP request to the /user/:id path
+
+
+app.use('/special', function(req, res){
+  //console.log(request(req.method,req.originalUrl,200))
+  res.send('Welcome to special!')
 })
 
+app.get('/news', function(req, res){
+  res.send('Welcome to news section');
+})
+
+app.use('/', function (req, res, next) {
+  //console.log(request(req.method,req.originalUrl,200))
+  //console.log(chalk.magenta('Successful Request :', req.method, 200 ,'OK'))
+  res.send('Welcome!')
+  next()
+})
 
 app.listen(3000, function(){
-  console.log('server listening');
+  console.log(chalk.blue('server listening'));
 } );
