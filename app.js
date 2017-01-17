24f32b47205244edc98c787e03a1cd30e9621343
Stephanie Cure <e.cure.ch@gmail.com>
2017-01-17 17:31:20 -0500

    Static Router added

--- app.js
+++ app.js
@@ -1,46 +1,32 @@
 const express = require('express');
 const app = express(); // creates an instance of an express application
 const chalk = require('chalk');
-const router = express.Router();
+const routes = require('./routes');
 const request = chalk.green;
 const morgan = require('morgan');
 const nunjucks = require('nunjucks');
 const people = [{name: 'Grace'}, {name: 'Hopper'}, {name: 'Daugther'}];
+
+
 app.set('view enginer', 'html');
+
 app.engine('html', nunjucks.render);
-
-
-//Loggin middleware
-//comment
 
 
 app.use(morgan('dev'))
 
-//a middleware sub-stack shows request info for any type of HTTP request to the /user/:id path
-
-
-app.use('/special', function(req, res){
-  //console.log(request(req.method,req.originalUrl,200))
-  res.send('Welcome to special!')
-})
-
-app.get('/news', function(req, res){
-  res.send('Welcome to news section');
-})
-
 app.use('/', function (req, res, next) {
-  //console.log(request(req.method,req.originalUrl,200))
-  //console.log(chalk.magenta('Successful Request :', req.method, 200 ,'OK'))
-  res.render( 'index.html', {title: 'Hall of Fame', people: people} );
+  res.render( 'index.html');
   next()
 })
+app.use('/', routes)
+app.use(express.static('public'));
+
 
 app.listen(3000, function(){
   console.log(chalk.blue('server listening'));
 } );
 
-
-// in some file that is in the root directory of our application... how about app.js?
 var locals = {
     title: 'An Example',
     people: [
@@ -50,8 +36,8 @@ var locals = {
     ]
 };
 
-
 nunjucks.configure('views', {noCache: true});
 nunjucks.render('index.html', locals, function (err, output) {
     console.log(output);
 });
+
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var tweetBank = require('../tweetBank');
+
+router.get('/', function (req, res) {
+  let tweets = tweetBank.list();
+  res.render( 'index', { tweets: tweets } );
+});
+
+module.exports = router;
+
+
+// replaced this hard-coded route with general static routing in app.js
+  // router.get('/stylesheets/style.css', function(req, res, next){
+  //   res.sendFile('/stylesheets/style.css', { root: __dirname + '/../public/' });
+  // });
